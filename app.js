66376a3c92382549fa0c24a82d88c604a2a13447
gaurigoyal28 <gaurigoyal2803@gmail.com>
2024-06-02 20:40:55 +0530

    CRUD operations completed

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path= require("path");
 const seedDB = require("./seed");
 const mongoose = require('mongoose');
 const productroutes = require("./routes/product");
+const ejsMate = require("ejs-mate");
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://127.0.0.1:27017/shopping_app')
 .then(()=>{
@@ -14,11 +16,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/shopping_app')
     console.log(err);
 })
 
+
+app.engine("ejs",ejsMate);
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname,'public')));
-
-
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 // seeding database
 
 // seedDB()
--- routes/product.js
+++ routes/product.js
@@ -7,4 +7,42 @@ router.get("/products", async(req,res)=>{
     res.render("products/index",{products});
 })
 
+router.get("/product/new",(req,res)=>{
+    res.render("products/new");
+
+})
+
+router.post("/products",async  (req,res)=>{
+    let {name,img,price,desc}=req.body;
+    await Product.create({name,img,price,desc});
+    
+    res.redirect("/products");
+
+})
+router.get("/products/:id", async(req,res)=>{
+    let {id} = req.params;
+    let foundProduct = await Product.findById(id);
+    res.render("products/show",{foundProduct});
+
+})
+
+router.get("/products/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    let foundProduct = await Product.findById(id);
+    res.render("products/edit", {foundProduct});
+})
+
+router.patch("/products/:id", async(req,res)=>{
+    let {id} = req.params;
+    let {name, img, price, desc} = req.body;
+    await Product.findByIdAndUpdate( id ,{name, img, price, desc} );
+    res.redirect(`/products/${id}`);
+})
+
+
+router.delete("/products/:id", async(req,res)=>{
+    let {id} = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect("/products");
+})
 module.exports = router;
